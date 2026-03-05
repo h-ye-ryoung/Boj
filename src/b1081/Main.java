1497d2e302a4e73f83a04d2e03e56d82410b7c05
김혜령 <hyeryoung27@soongsil.ac.kr>
2026-03-05 17:55:23 +0900

    feat: 10814

---
+++ src/b1081/Main.java
@@ -0,0 +1,39 @@
+package b1081;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        ArrayList<Member> array = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            array.add(new Member());
+            array.get(i).age = Integer.parseInt(st.nextToken());
+            array.get(i).name = st.nextToken();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 200; i++) {
+            for (int j = 0; j < N; j++) {
+                if (array.get(j).age == i) {
+                    sb.append(array.get(j).age).append(" ").append(array.get(j).name).append("\n");
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+
+    public static class Member {
+        int age;
+        String name;
+    }
+}
