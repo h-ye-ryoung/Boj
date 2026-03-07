8a3c9a3cb03dff740a089de18a71a582cc86a2a9
김혜령 <hyeryoung27@soongsil.ac.kr>
2026-03-07 11:58:38 +0900

    feat: 10773

---
+++ src/b10773/Main.java
@@ -0,0 +1,30 @@
+package b10773;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < K; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.add(num);
+            }
+        }
+
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+
+        System.out.println(result);
+    }
+}
